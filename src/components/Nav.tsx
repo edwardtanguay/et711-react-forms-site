51d754773ae2e88ddeffa196eaaf7820470d6f6c
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-12 22:52:32 +0100

    simple form in

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,19 @@
 import { NavLink } from "react-router-dom";
 
 export const Nav = () => {
-	return <nav>
-		<ul className="flex gap-4 bg-slate-500 p-1 content">
-			<li><NavLink to="/simple-form">Simple Form</NavLink></li>
-			<li><NavLink to="/info">Info</NavLink></li>
-			<li><NavLink to="/about">About</NavLink></li>
-		</ul>
-	</nav>;
+	return (
+		<nav>
+			<ul className="flex gap-4 bg-slate-500 px-4 py-2 content">
+				<li>
+					<NavLink to="/simple-form">Simple Form</NavLink>
+				</li>
+				<li>
+					<NavLink to="/info">Info</NavLink>
+				</li>
+				<li>
+					<NavLink to="/about">About</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
 };
--- src/pages/PageSimpleForm.tsx
+++ src/pages/PageSimpleForm.tsx
@@ -1,5 +1,77 @@
+import { FormEvent } from "react";
+
 export const PageSimpleForm = () => {
+	const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.target as HTMLFormElement);
+		console.log(data);
+	};
+
 	return (
-		<p>this will be the simple form</p>
+		<form onSubmit={handleFormSubmit}>
+			<fieldset className="border border-gray-500 p-4 w-full sm:w-40 rounded">
+				<legend>New Employee</legend>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="firstName">
+						First Name:
+					</label>
+					<input
+						type="text"
+						id="firstName"
+						name="firstName"
+						required
+					/>
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="lastName">
+						Last Name:
+					</label>
+					<input type="text" id="lastName" name="lastName" required />
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="age">
+						Age:
+					</label>
+					<input type="number" id="age" name="age" required />
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="hireDate" >
+						Hire Date:
+					</label>
+					<input type="date" id="hireDate" name="hireDate" required />
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="employeeNumber">
+						Employee Number:
+					</label>
+					<div>
+						<input
+							type="text"
+							id="employeeNumber"
+							name="employeeNumber"
+							pattern="^[DSM]-\d{4}$"
+							required
+						/>
+						<p className="text-sm">e.g. D-2832, S-7771, M-8123</p>
+					</div>
+				</div>
+
+				<div className="mb-4 flex gap-2">
+					<label className="w-[10rem]" htmlFor="notes" >
+						Notes:
+					</label>
+					<textarea id="notes" name="notes"></textarea>
+				</div>
+
+				<div className="mt-5 flex justify-end pr-3">
+					<button>Add Employee</button>
+				</div>
+			</fieldset>
+		</form>
 	);
 };
