6e3f8516f2f03750d428c8ff528c1021f1f83f1f
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-13 02:39:56 +0100

    hiding page

--- src/pages/PageSimpleForm.tsx
+++ src/pages/PageSimpleForm.tsx
@@ -35,71 +35,91 @@ export const PageSimpleForm = () => {
 	};
 
 	return (
-		<form onSubmit={handleFormSubmit}>
-			<p>environment is: [{config.environment}]</p>
-			<fieldset className="border border-gray-500 p-4 w-full sm:w-40 rounded">
-				<legend>New Employee</legend>
+		<>
+			{config.environment !== "development" ? (
+				<p>this page is not available</p>
+			) : (
+				<form onSubmit={handleFormSubmit}>
+					<fieldset className="border border-gray-500 p-4 w-full sm:w-40 rounded">
+						<legend>New Employee</legend>
 
-				<div className="mb-4 flex gap-2">
-					<label className="w-[10rem]" htmlFor="firstName">
-						First Name:
-					</label>
-					<input
-						type="text"
-						id="firstName"
-						name="firstName"
-						required
-					/>
-				</div>
+						<div className="mb-4 flex gap-2">
+							<label className="w-[10rem]" htmlFor="firstName">
+								First Name:
+							</label>
+							<input
+								type="text"
+								id="firstName"
+								name="firstName"
+								required
+							/>
+						</div>
 
-				<div className="mb-4 flex gap-2">
-					<label className="w-[10rem]" htmlFor="lastName">
-						Last Name:
-					</label>
-					<input type="text" id="lastName" name="lastName" required />
-				</div>
+						<div className="mb-4 flex gap-2">
+							<label className="w-[10rem]" htmlFor="lastName">
+								Last Name:
+							</label>
+							<input
+								type="text"
+								id="lastName"
+								name="lastName"
+								required
+							/>
+						</div>
 
-				<div className="mb-4 flex gap-2">
-					<label className="w-[10rem]" htmlFor="age">
-						Age:
-					</label>
-					<input type="number" id="age" name="age" required />
-				</div>
+						<div className="mb-4 flex gap-2">
+							<label className="w-[10rem]" htmlFor="age">
+								Age:
+							</label>
+							<input type="number" id="age" name="age" required />
+						</div>
 
-				<div className="mb-4 flex gap-2">
-					<label className="w-[10rem]" htmlFor="hireDate">
-						Hire Date:
-					</label>
-					<input type="date" id="hireDate" name="hireDate" required />
-				</div>
+						<div className="mb-4 flex gap-2">
+							<label className="w-[10rem]" htmlFor="hireDate">
+								Hire Date:
+							</label>
+							<input
+								type="date"
+								id="hireDate"
+								name="hireDate"
+								required
+							/>
+						</div>
 
-				<div className="mb-4 flex gap-2">
-					<label className="w-[10rem]" htmlFor="employeeNumber">
-						Employee Number:
-					</label>
-					<div>
-						<input
-							type="text"
-							id="employeeNumber"
-							name="employeeNumber"
-							pattern="^[DSM]-\d{4}$"
-							required
-						/>
-						<p className="text-sm">e.g. D-2832, S-7771, M-8123</p>
-					</div>
-				</div>
+						<div className="mb-4 flex gap-2">
+							<label
+								className="w-[10rem]"
+								htmlFor="employeeNumber"
+							>
+								Employee Number:
+							</label>
+							<div>
+								<input
+									type="text"
+									id="employeeNumber"
+									name="employeeNumber"
+									pattern="^[DSM]-\d{4}$"
+									required
+								/>
+								<p className="text-sm">
+									e.g. D-2832, S-7771, M-8123
+								</p>
+							</div>
+						</div>
 
-				<div className="mb-4 flex gap-2">
-					<label className="w-[10rem]" htmlFor="notes">
-						Notes:
-					</label>
-					<textarea id="notes" name="notes"></textarea>
-				</div>
+						<div className="mb-4 flex gap-2">
+							<label className="w-[10rem]" htmlFor="notes">
+								Notes:
+							</label>
+							<textarea id="notes" name="notes"></textarea>
+						</div>
 
-				<div className="mt-5 flex justify-end pr-3">
-					<button>Add Employee</button>
-				</div>
-			</fieldset>
-		</form>
+						<div className="mt-5 flex justify-end pr-3">
+							<button>Add Employee</button>
+						</div>
+					</fieldset>
+				</form>
+			)}
+		</>
 	);
 };
